f928cb611f9519fc3dbb52cfde6b408da567ce64
ly0 <ly0@protonmail.com>
2026-02-11 00:02:49 +0100

    more operators

---
+++ getopsbyname.py
@@ -0,0 +1,395 @@
+names = [
+    "AltaLink", "ATCO", "BC Hydro", "Emera Incorporated", "ENMAX Power Corporation",
+    "Hydro One", "Hydro-Québec", "Manitoba Hydro", "Nalcor Energy", "New Brunswick Power (NB Power)",
+    "Nova Scotia Power Incorporated (NSPI)", "SaskPower",
+    "Ameren Corporation", "American Electric Power (AEP)", "American Transmission Company (ATC)",
+    "Avangrid (Iberdrola USA)", "Bonneville Power Administration (BPA)", "CentrePoint Energy",
+    "Consolidated Edison (ConEd)", "Dominion Resource", "Duke Energy", "Entergy Corporation",
+    "Eversource Energy (Northeast Utilities earlier)", "Exelon Corporation", "FirstEnergy Corporation",
+    "ITC Holdings", "MidAmerican Energy Company", "National Grid USA", "New York Power Authority (NYPA)",
+    "NextEra Energy", "NorthWestern Energy", "NV Energy", "Oklahoma Gas and Electric Company (OG&E)",
+    "Oncor Electric Delivery", "Pacific Gas and Electric Corporation (PG&E)", "PacifiCorp",
+    "PNM Resources", "PPL Corporation", "Public Service Electric & Gas (PSE&G)",
+    "Southern California Edison (SCE)", "Southern Company", "Tennessee Valley Authority (TVA)", "Xcel Energy",
+    "Compañía de Transporte de Energía Eléctrica en Alta Tensión Transener S.A. (Transener)",
+    "Empresa Nacional de Electricidad (ENDE)", "ENDE Transmission", "ISA Bolivia S.A. (ISA Bolivia)",
+    "EDP Transmissão Goiás S.A. (EDP Goiás)", "Companhia Hidro Elétrica do São Francisco (Chesf)",
+    "Companhia Energetica de Minas Gerais (Cemig)", "Companhia Estadual de Transmissão Energia Elétrica SA (CEEE-T)",
+    "Companhia de Transmissão de Energia Elétrica Paulista (CTEEP)", "Copel Geração e Transmissão S.A. (Copel GeT)",
+    "Centrais Elétricas Brasileiras S.A. (Eletrobrás)", "Centrais Elétricas do Norte do Brasil S.A. (Eletronorte)",
+    "Companhia de Geração e Transmissão de Energia Elétrica do Sul do Brasil (CGT Eletrosul)",
+    "Furnas Centrais Elétricas S.A. (Furnas)", "State Grid Brazil Holding S.A. (SGBH)",
+    "Transmissora Aliança de Energia Elétrica SA (TAESA)",
+    "AES Gener S.A.", "ENGIE Energía Chile S.A", "Transelec S.A.", "Sociedad Austral de Electricidad S.A. (SAESA)",
+    "Grupo Energía Bogotá (GEB)", "Celsia Colombia S.A. E.S.P",
+    "Empresas Públicas de Medellín E.S.P. (EPM)", "Interconexión Eléctrica S.A. (ISA)", "Transelca S.A.",
+    "Instituto Costarricense de Electricidad (ICE)",
+    "TRANSELECTRIC S.A.",
+    "Empresa de Transporte y Control de Energía Eléctrica (ETCEE)", "Fersa Sociedad Anónima (Fersa SA)",
+    "Transportadora de Energía de Centroamérica S.A. (TRECSA)", "Transportista Eléctrica Centroamericana S. A. (TRELEC)",
+    "Empresa Nacional de Energía Eléctrica (ENEE)",
+    "Comisión Federal de Electricidad (CFE)",
+    "Administración Nacional de Electricidad (ANDE)",
+    "Empresa de Transmisión Eléctrica S.A. (ETESA)",
+    "Abengoa Peru S.A.", "Consorcio Energético de Huancavelica S.A. (CONENHUA)",
+    "Consorcio Transmantaro S.A. (CTM)", "Etenorte S.R.L.", "Eteselva S.R.L.", "ISA Perú S.A.",
+    "Red de Energía del Perú S.A. (REP SA)", "Red Eléctrica del Sur S.A. (Redesur)",
+    "Administración Nacional de Usinas y Trasmisiones Eléctricas (UTE)",
+    "AusNet Services (Transmission) Ltd", "ElectraNet Pty Ltd", "Powerlink Queensland",
+    "Tasmanian Networks Pty Ltd (TasNetworks)", "TransGrid", "Western Power",
+    "Azerenergy",
+    "Electricité du Cambodge (EDC)",
+    "China Southern Power Grid (CSG)", "State Grid Corporation of China (SGCC)",
+    "Gujarat Electricity Transmission Company Limited (GETCO)", "Karnataka Power Transmission Corporation Limited (KPTCL)",
+    "Madhya Pradesh Power Transmission Company Limited (MPPTCL)", "Maharashtra State Electricity Transmission Company Limited (MSETCL)",
+    "Powergrid Corporation of India Limited (POWERGRID)", "Rajasthan Rajya Vidyut Prasaran Nigam Limited (RVPNL)",
+    "Transmission Corporation of Andhra Pradesh Limited (APTransco)", "Transmission Corporation of Telangana Limited (TSTRANSCO)",
+    "Tamil Nadu Transmission Corporation Limited (TANTRANSCO)", "Uttar Pradesh Power Transmission Corporation Limited (UPPTCL)",
+    "Adani Power Transmission (APL)", "Sterlite Power Transmission Limited (SPTL)",
+    "Perusahaan Listrik Negara (PLN)",
+    "Chubu Electric Power Grid Company, Inc", "Chugoku Electric Power Company, Inc", "Hokkaido Electric Power Network Company Limited",
+    "Hokuriku Electric Power Company, Inc", "J‐POWER (Electric Power Development Company Limited)", "Kansai Electric Power Company, Inc",
+    "Kyushu Electric Power Company, Inc", "Okinawa Electric Power Company, Inc", "Shikoku Electric Power Company, Inc",
+    "Tohoku Electric Power Network Company, Inc", "TEPCO Power Grid, Inc",
+    "Kazakhstan Electricity Grid Operating Company (KEGOC)",
+    "Electricite du Laos (EDL)",
+    "Sabah Electricity SdnBhd (SESB)", "Syarikat SESCO Berhad (SESCO)", "Tenaga Nasional Berhad (TNB)",
+    "Nepal Electricity Authority (NEA)",
+    "K-Electric", "National Transmission and Despatch Company (NTDC)",
+    "National Grid Corporation of the Philippines (NGCP)",
+    "Singapore Power (SP)",
+    "Korea Electric Power Corporation (KEPCO)",
+    "Electricity Generating Authority of Thailand (EGAT)",
+    "Uzbekenergo",
+    "EVN National Power Transmission Corporation (EVN NPT)",
+    "Austrian Power Grid AG (APG)", "TINETZ-TirolerNetze GmbH", "Vorarlberg Übertragungsnetz GmbH (VÜN)",
+    "Elia System Operator SA (Elia)",
+    "Independent System Operator in Bosnia and Herzegovina (ISO B&H)",
+    "Electricity System Operator (ESO)",
+    "Hrvatskog operatora prijenosnog sustava d.o.o. (HOPS)",
+    "ČEPS A.S.",
+    "Energinet.dk",
+    "Elering OÜ",
+    "FingridOyj",
+    "Reseau de Transport d'Electricite (RTE)",
+    "Georgia State Electrosystem JSC (GSE)",
+    "50Hertz Transmission GmbH (50Hertz)", "Amprion GmbH", "TenneT TSO GmbH", "TransnetBW GmbH",
+    "Anexartitos Diacheiristis Metaforas Ilektrikis Energeias A.E. (ADMIE)",
+    "MAVIR ZRt.",
+    "Landsnet HF",
+    "EirGrid Plc",
+    "Terna S.p.A",
+    "Augstsprieguma Tīkls AS (AST)",
+    "Litgrid AB",
+    "Makedonski Elektro Prenosen Sistem Operator A.D. (MEPSO)",
+    "Crnogorski Elektroprenosni Sistem A.D. (CGES)",
+    "TenneT TSO B.V.",
+    "Statnett SF",
+    "Polskie Sieci Elektroenergetyczne S.A. (PSE)",
+    "Rede Eléctrica Nacional (REN)",
+    "Rede Eléctrica Nacional SA (REN)", "Red Eléctrica de España S.A. (REE)",
+    "Transelectrica S.A.",
+    "Elektromreža Srbije (EMS)",
+    "Slovenska Elektrizacna Prenosova Sustava AS (SEPS)",
+    "ELES d.o.o",
+    "Svenska Kraftnät",
+    "Swissgrid AG",
+    "Türkiye ElektrikIletim A.Ş. (TEIAS)",
+    "National Grid Electricity Transmission Plc (NGET)", "Scottish Hydro Electric Transmission Limited (SHETL)", "Scottish Power Transmission Plc (SPT)",
+    "Electricity and Water Authority (EWA)",
+    "National Electric Power Company (NEPCO)",
+    "Ministry of Electricity and Water (MEW)",
+    "Oman Electricity Transmission Company (OETC)",
+    "Qatar General Electricity & Water Corporation (KAHRAMAA)",
+    "Saudi Electricity Company (SEC)",
+    "Dubai Electricity and Water Authority (DEWA)", "Abu Dhabi Transmission and Despatch Company (TRANSCO)",
+    "Société Algérienne de Gestion du Réseau de Transport de l’Electricité (GTRE)",
+    "Rede Nacional de Transporte de Electricidade (RNT)",
+    "Botswana Power Corporation (BPC)",
+    "SONATREL",
+    "Egyptian Electricity Transmission Company (EETC)",
+    "Ethiopian Electric Power Corporation (EEPCo)",
+    "Ghana Grid Company Limited (GRIDCo)", "Transpower",
+    "Compangie Ivoirienne d’Electricité (CIE)",
+    "Kenya Power", "Kenya Electricity Transmission Company Limited (KETRACO)",
+    "L’Office National d’Electricité et de l’Eau Potable (ONEE)",
+    "Electricidade de Moçambique (EDM)",
+    "Namibia Power Corporation (Proprietary) Limited (NamPower)",
+    "Transmission Company of Nigeria (TCN)",
+    "Rwanda Energy Group Limited (REG)",
+    "Eskom Holdings SOC Limited (Eskom)",
+    "Tanzania Electric Supply Company (TANESCO)",
+    "Société Tunisienne de l’Electricité et du Gaz (STEG)",
+    "Uganda Electricity Transmission Company Limited (UETCL)",
+    "ZESCO Limited",
+    # New names
+    "OST sh.a", "Austrian Power Grid AG", "Vorarlberger Übertragungsnetz GmbH", "NOS BiH",
+    "Elia System Operator SA", "Electroenergien Sistemen Operator EAD", "Swissgrid ag",
+    "Cyprus Transmission System Operator", "ČEPS a.s.", "TransnetBW GmbH",
+    "TenneT TSO GmbH", "Amprion GmbH", "50Hertz Transmission GmbH", "Energinet",
+    "Elering AS", "Red Eléctrica de España S.A.", "Fingrid Oyj", "Réseau de Transport d'Electricité",
+    "Independent Power Transmission Operator S.A.", "HOPS d.d.", "MAVIR", "EirGrid plc",
+    "Landsnet hf", "Terna - Rete Elettrica Nazionale SpA", "Litgrid AB", "Creos Luxembourg S.A.",
+    "AS Augstsprieguma tīkls", "Crnogorski Elektroprenosni Sistem AD", "System Operator for Northern Ireland Ltd",
+    "TenneT TSO B.V.", "Statnett SF", "Transmission System Operator of the Republic of North Macedonia",
+    "Polskie Sieci Elektroenergetyczne S.A.", "Rede Eléctrica Nacional, S.A.", "C.N. Transelectrica S.A.",
+    "Akcionarsko društvo Elektromreža Srbije", "Svenska Kraftnät", "ELES, d.o.o.", 
+    "Slovenská elektrizačná prenosová sústava, a.s.", "National Power Company Ukrenergo",
+    "Moldelectrica", "Turkish Electricity Transmission Corporation"
+]
+
+
+import pandas as pd
+import requests
+from SPARQLWrapper import SPARQLWrapper, JSON
+from difflib import SequenceMatcher
+import time
+
+# ----------------------------
+# Part 1: Collect operators by types
+# ----------------------------
+
+ENDPOINT_URL = "https://query.wikidata.org/sparql"
+
+sparql = SPARQLWrapper(ENDPOINT_URL)
+sparql.setReturnFormat(JSON)
+
+query = """
+SELECT
+  ?operator ?operatorLabel
+  ?operatorType ?operatorTypeLabel
+  ?country ?countryLabel
+  ?website
+  ?hqLabel
+  ?inception
+  ?ceoLabel
+  ?employees
+  ?revenue
+  ?industryLabel
+  ?logo
+  ?stockExchangeLabel
+  ?dissolved
+WHERE {
+  VALUES ?operatorType {
+    wd:Q112046  # transmission system operator
+    wd:Q472093  # electricity distribution network operator
+  }
+
+  ?operator wdt:P31 ?operatorType ;
+            wdt:P17 ?country .
+
+  OPTIONAL { ?operator wdt:P856 ?website . }
+  OPTIONAL { ?operator wdt:P159 ?hq . }
+  OPTIONAL { ?operator wdt:P571 ?inception . }
+  OPTIONAL { ?operator wdt:P169 ?ceo . }
+  OPTIONAL { ?operator wdt:P1128 ?employees . }
+  OPTIONAL { ?operator wdt:P2139 ?revenue . }
+  OPTIONAL { ?operator wdt:P452 ?industry . }
+  OPTIONAL { ?operator wdt:P154 ?logo . }
+  OPTIONAL { ?operator wdt:P414 ?stockExchange . }
+  OPTIONAL { ?operator wdt:P576 ?dissolved . }
+
+  SERVICE wikibase:label {
+    bd:serviceParam wikibase:language "en".
+  }
+}
+ORDER BY ?countryLabel ?operatorLabel
+"""
+
+sparql.setQuery(query)
+results = sparql.query().convert()["results"]["bindings"]
+
+rows = []
+for r in results:
+    rows.append({
+        "operator_qid": r["operator"]["value"].split("/")[-1],
+        "operator_label": r["operatorLabel"]["value"],
+        "operator_type_qid": r["operatorType"]["value"].split("/")[-1],
+        "operator_type_label": r["operatorTypeLabel"]["value"],
+        "country_qid": r["country"]["value"].split("/")[-1],
+        "country_label": r["countryLabel"]["value"],
+        "website": r.get("website", {}).get("value"),
+        "headquarters": r.get("hqLabel", {}).get("value"),
+        "inception": r.get("inception", {}).get("value"),
+        "ceo": r.get("ceoLabel", {}).get("value"),
+        "employees": r.get("employees", {}).get("value"),
+        "revenue": r.get("revenue", {}).get("value"),
+        "industry": r.get("industryLabel", {}).get("value"),
+        "logo": r.get("logo", {}).get("value"),
+        "stock_exchange": r.get("stockExchangeLabel", {}).get("value"),
+        "dissolved": r.get("dissolved", {}).get("value")
+    })
+
+df_types = pd.DataFrame(rows)
+df_types = df_types.drop_duplicates(subset="operator_qid", keep="first")
+print(f"✅ Retrieved {len(df_types)} unique operators by type.")
+
+# ----------------------------
+# Part 2: Collect operators by names
+# ----------------------------
+
+def similarity(a, b):
+    return SequenceMatcher(None, a.lower(), b.lower()).ratio()
+
+def search_wikidata(name, limit=10, fuzzy_threshold=0.6, max_fallbacks=3):
+    def _search(query):
+        url = "https://www.wikidata.org/w/api.php"
+        params = {
+            "action": "wbsearchentities",
+            "search": query,
+            "language": "en",
+            "format": "json",
+            "type": "item",
+            "limit": limit
+        }
+        headers = {"User-Agent": "MyWikidataScript/1.0 (your_email@example.com)"}
+        response = requests.get(url, params=params, headers=headers)
+        try:
+            data = response.json()
+        except Exception as e:
+            print(f"Error decoding JSON for '{query}': {e}")
+            return []
+        
+        results = []
+        for item in data.get("search", []):
+            label = item["label"]
+            score = similarity(query, label)
+            if score >= fuzzy_threshold:
+                results.append((item["id"], label, score))
+        results.sort(key=lambda x: x[2], reverse=True)
+        return results
+
+    current_name = name
+    attempts = 0
+    while current_name and attempts <= max_fallbacks:
+        results = _search(current_name)
+        if results:
+            return results
+        if " " in current_name:
+            current_name = " ".join(current_name.split()[:-1])
+            attempts += 1
+        else:
+            break
+    return []
+
+# Search by names
+wikidata_name_results = []
+for name in names:
+    results = search_wikidata(name)
+    if results:
+        for entity_id, label, score in results:
+            wikidata_name_results.append({
+                "operator_name": name,
+                "operator_qid": entity_id,
+                "operator_label": label,
+                "similarity_score": score
+            })
+    else:
+        wikidata_name_results.append({
+            "operator_name": name,
+            "operator_qid": None,
+            "operator_label": None,
+            "similarity_score": None
+        })
+print(wikidata_name_results)
+df_names = pd.DataFrame(wikidata_name_results)
+
+# ----------------------------
+# Part 3: Fetch missing metadata from Wikidata for new QIDs
+# ----------------------------
+
+def fetch_operator_metadata(qid):
+    """SPARQL fetch operator info by QID"""
+    if not qid:
+        return None
+    query = f"""
+    SELECT
+      ?operator ?operatorLabel
+      ?operatorType ?operatorTypeLabel
+      ?country ?countryLabel
+      ?website
+      ?hqLabel
+      ?inception
+      ?ceoLabel
+      ?employees
+      ?revenue
+      ?industryLabel
+      ?logo
+      ?stockExchangeLabel
+      ?dissolved
+    WHERE {{
+      BIND(wd:{qid} AS ?operator)
+      OPTIONAL {{ ?operator wdt:P31 ?operatorType. }}
+      OPTIONAL {{ ?operator wdt:P17 ?country. }}
+      OPTIONAL {{ ?operator wdt:P856 ?website. }}
+      OPTIONAL {{ ?operator wdt:P159 ?hq. }}
+      OPTIONAL {{ ?operator wdt:P571 ?inception. }}
+      OPTIONAL {{ ?operator wdt:P169 ?ceo. }}
+      OPTIONAL {{ ?operator wdt:P1128 ?employees. }}
+      OPTIONAL {{ ?operator wdt:P2139 ?revenue. }}
+      OPTIONAL {{ ?operator wdt:P452 ?industry. }}
+      OPTIONAL {{ ?operator wdt:P154 ?logo. }}
+      OPTIONAL {{ ?operator wdt:P414 ?stockExchange. }}
+      OPTIONAL {{ ?operator wdt:P576 ?dissolved. }}
+      SERVICE wikibase:label {{ bd:serviceParam wikibase:language "en". }}
+    }}
+    """
+    sparql.setQuery(query)
+    try:
+        result = sparql.query().convert()["results"]["bindings"]
+        if not result:
+            return None
+        r = result[0]
+        return {
+            "operator_qid": qid,
+            "operator_label": r.get("operatorLabel", {}).get("value"),
+            "operator_type_qid": r.get("operatorType", {}).get("value", "").split("/")[-1] if r.get("operatorType") else None,
+            "operator_type_label": r.get("operatorTypeLabel", {}).get("value"),
+            "country_qid": r.get("country", {}).get("value", "").split("/")[-1] if r.get("country") else None,
+            "country_label": r.get("countryLabel", {}).get("value"),
+            "website": r.get("website", {}).get("value"),
+            "headquarters": r.get("hqLabel", {}).get("value"),
+            "inception": r.get("inception", {}).get("value"),
+            "ceo": r.get("ceoLabel", {}).get("value"),
+            "employees": r.get("employees", {}).get("value"),
+            "revenue": r.get("revenue", {}).get("value"),
+            "industry": r.get("industryLabel", {}).get("value"),
+            "logo": r.get("logo", {}).get("value"),
+            "stock_exchange": r.get("stockExchangeLabel", {}).get("value"),
+            "dissolved": r.get("dissolved", {}).get("value")
+        }
+    except Exception as e:
+        print(f"Error fetching metadata for {qid}: {e}")
+        return None
+
+# Find new QIDs from name search that are not in type search
+new_qids = set(df_names["operator_qid"]) - set(df_types["operator_qid"])
+metadata_rows = []
+for qid in new_qids:
+    data = fetch_operator_metadata(qid)
+    if data:
+        metadata_rows.append(data)
+    time.sleep(0.1)  # polite delay
+
+df_new_metadata = pd.DataFrame(metadata_rows)
+
+# ----------------------------
+# Part 4: Combine datasets
+# ----------------------------
+
+df_combined = pd.concat([df_types, df_new_metadata], ignore_index=True)
+df_combined = df_combined.drop_duplicates(subset="operator_qid", keep="first")
+
+# Optional: merge similarity info from name search
+df_combined = df_combined.merge(
+    df_names[["operator_qid", "similarity_score", "operator_name"]],
+    on="operator_qid",
+    how="left"
+)
+
+# ----------------------------
+# Save final combined CSV
+# ----------------------------
+
+df_combined.to_csv("grid_operators_combined.csv", index=False)
+print(f"✅ Combined dataset saved with {len(df_combined)} operators to 'grid_operators_combined.csv'.")
